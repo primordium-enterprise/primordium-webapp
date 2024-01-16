c9cbbc962194593686664a9f8b77a183a64aeb40
BCJdevelopment <bcj@bcjdevelopment.com>
2024-01-16 05:18:37 -0700

    started disconnect option for disconnecting wallet

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,19 +1,34 @@
 'use client'
 
 import { ConnectWalletModalContext } from "@/modals/ConnectWalletModal";
-import { Button } from "@nextui-org/react";
+import { Button, Dropdown, DropdownTrigger, DropdownMenu, DropdownItem } from "@nextui-org/react";
 import { useContext } from "react";
-import { useAccount } from "wagmi";
+import { useAccount, useDisconnect } from "wagmi";
 
 export default function Navigation() {
   const connectWalletModal = useContext(ConnectWalletModalContext);
 
-  const { isConnected } = useAccount();
+  const { address, isConnected } = useAccount();
+  const { disconnect } = useDisconnect();
 
   return (
     <nav className="flex justify-end content-center px-2 py-4">
       {isConnected ? <>
-
+        <Dropdown>
+          <DropdownTrigger>
+            <Button variant="bordered">{address}</Button>
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Account Actions">
+            <DropdownItem
+              key="disconnect"
+              className="text-danger"
+              color="danger"
+              onClick={() => disconnect()}
+            >
+              Disconnect
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
       </> : <>
         <Button variant="ghost" color="default" onClick={connectWalletModal.onOpen}>Connect Wallet</Button>
       </>}
