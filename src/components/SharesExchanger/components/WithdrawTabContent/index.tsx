2e642eff38c713f570105783cb618a0c16ad6b59
BCJdevelopment <bcj@bcjdevelopment.com>
2024-01-23 04:15:36 -0700

    withdraw amount conditionals + slider

--- src/components/SharesExchanger/components/WithdrawTabContent/index.tsx
+++ src/components/SharesExchanger/components/WithdrawTabContent/index.tsx
@@ -2,33 +2,87 @@ import AssetAmountInput from "@/components/AssetAmountInput";
 import primordiumContracts from "@/config/primordiumContracts";
 import { ChooseWalletModalContext } from "@/context/ChooseWalletModal";
 import useFormattedBalance from "@/hooks/useFormattedBalance";
-import { Button } from "@nextui-org/react";
-import { useContext, useState } from "react";
+import parseDnumFromString from "@/utils/parseDnumFromString";
+import { Button, Slider, input, slider } from "@nextui-org/react";
+import { useContext, useMemo, useState } from "react";
 import { useAccount } from "wagmi";
+import { format as dnFormat } from "dnum";
 
 const token = primordiumContracts.token.address;
+const MULT = 1000;
 
 export default function WithdrawTabContent() {
   const { address, isConnected } = useAccount();
-  const { value: mushiBalance } = useFormattedBalance({ address, token });
+  const { value: mushiBalance, decimals } = useFormattedBalance({ address, token });
 
   const { onOpen } = useContext(ChooseWalletModalContext);
 
-  const [withdrawValue, setWithdrawValue] = useState("");
+  const [withdrawInputValue, setWithdrawInputValue] = useState("");
+  const [withdrawValue, setWithdrawValue] = useState(BigInt(0));
+  const onWithdrawInputValueChange = (inputValue: string) => {
+    let value = parseDnumFromString(inputValue, decimals)[0];
+    setWithdrawValue(value);
+    if (mushiBalance > 0) {
+      setSliderValue(
+        Number((value * BigInt(MULT)) / mushiBalance) / MULT,
+      );
+    }
+    setWithdrawInputValue(inputValue);
+  };
+
+  const [sliderValue, setSliderValue] = useState(0);
+  const onSliderValueUpdate = (sliderValue: number | number[]) => {
+    if (sliderValue === 1) {
+      let value = mushiBalance;
+      setWithdrawValue(value);
+      setWithdrawInputValue(dnFormat([value, decimals]));
+    } else if (sliderValue === 0) {
+      setWithdrawValue(BigInt(0));
+      setWithdrawInputValue("0");
+    } else {
+      let value =
+        (BigInt((sliderValue as number) * MULT) * mushiBalance) /
+        BigInt(MULT);
+      setWithdrawValue(value);
+      setWithdrawInputValue(dnFormat([value, decimals]));
+    }
+    setSliderValue(sliderValue as number);
+  };
+
+  const [isWithdrawAmount, isInsufficientBalance] = useMemo(() => {
+    return [withdrawValue > 0, withdrawValue > mushiBalance];
+  }, [withdrawValue, mushiBalance]);
+
+  const isReady = useMemo(() => {
+    return isWithdrawAmount && !isInsufficientBalance;
+  }, [isWithdrawAmount, isInsufficientBalance]);
+
   return isConnected ? (
     <>
       <p className="mb-2 text-sm text-default-400">
-        <span className="text-warning-300">NOTE:</span> Withdrawing burns your MUSHI share tokens.
-        Only proceed if you wish to dissolve some or all of your membership:
+        <span className="text-warning-400">WAIT, THINK TWICE!</span> Withdrawing burns your MUSHI
+        share tokens. Only proceed if you wish to dissolve some or all of your membership:
       </p>
       <AssetAmountInput
-        value={withdrawValue}
-        onValueChange={setWithdrawValue}
-        label="Mint amount"
+        value={withdrawInputValue}
+        onValueChange={onWithdrawInputValueChange}
+        label="Withdraw amount"
         token={token}
       />
-      <Button color="secondary" size="lg" fullWidth>
-        {"Withdraw shares"}
+      {mushiBalance > 0 && (
+        <Slider
+          size="sm"
+          step={1 / MULT}
+          minValue={0}
+          maxValue={1}
+          hideValue
+          value={sliderValue}
+          onChange={onSliderValueUpdate}
+          aria-label="Withdraw amount slider"
+        />
+      )}
+      <Button className="mt-2" color="warning" size="lg" fullWidth isDisabled={!isReady}>
+        {isInsufficientBalance ? "Insufficient Balance" : "Withdraw shares"}
       </Button>
     </>
   ) : (
--- src/utils/parseDnumFromString.ts
+++ src/utils/parseDnumFromString.ts
@@ -1,16 +1,18 @@
 import { Dnum, from } from "dnum";
 
-export default function parseDnumFromString(value: string): Dnum {
-  let end = value.length - 1;
-
+export default function parseDnumFromString(value: string, decimals: number = 18): Dnum {
   if (!value) {
     value = "0"
-  } else if (value[end] == ".") {
-    if (end === 0) {
-      value = "0";
-    } else {
-      value = value.slice(0, end);
+  } else {
+    let end = value.length - 1;
+    if (value[end] == ".") {
+      if (end === 0) {
+        value = "0";
+      } else {
+        value = value.slice(0, end);
+      }
     }
   }
-  return from(value, 18);
+
+  return from(value, decimals);
 };
