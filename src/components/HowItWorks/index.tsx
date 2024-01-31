e6b1893e0bbc9aa5a88746d7948d68f09b811e76
BCJdevelopment <bcj@bcjdevelopment.com>
2024-01-30 23:55:42 -0700

    working on list item styling

--- src/components/HowItWorks/index.tsx
+++ src/components/HowItWorks/index.tsx
@@ -11,11 +11,24 @@ const accordianItems: {
     content: (
       <ul>
         <li>
-          To become a founding member, mint MUSHI membership tokens by&nbsp;
-          <Link href="/exchange">depositing ETH</Link>.
+          To become a founding member, mint MUSHI membership tokens by{" "}
+          <Link href="/exchange">depositing ETH.</Link>
         </li>
         <li>
           <b>100% of ETH deposits</b>&nbsp;go to the DAO treasury.
+        </li>
+        <li>
+          MUSHI holders can <Link href="/exchange?tab=withdraw">withdraw</Link> (”rage-quit”){" "}
+          <b>at any time,</b> burning their MUSHI tokens and receiving their pro-rata share of ETH
+          and/or ERC20 assets from the treasury.
+        </li>
+        <li>
+          There is <b>no other way to receive MUSHI tokens.</b> No tokens are airdropped or minted
+          for free,{" "}
+          <b>
+            not even to the <Link href="https://bcjdevelopment.com">DAO creators.</Link>
+          </b>{" "}
+          Everyone has equal access at the same equal share price.
         </li>
       </ul>
     ),
@@ -24,9 +37,14 @@ const accordianItems: {
 
 export default function HowItWorks() {
   return (
-    <Accordion defaultExpandedKeys={accordianItems.map(item => item.title)}>
+    <Accordion defaultExpandedKeys={accordianItems.map((item) => item.title)}>
       {accordianItems.map((item) => (
-        <AccordionItem key={item.title} title={item.title} aria-label={`Accordion - ${item.title}`}>
+        <AccordionItem
+          key={item.title}
+          title={item.title}
+          aria-label={`Accordion - ${item.title}`}
+          classNames={{ title: "text-base", content: "pl-1 text-foreground-600" }}
+        >
           {item.content}
         </AccordionItem>
       ))}
