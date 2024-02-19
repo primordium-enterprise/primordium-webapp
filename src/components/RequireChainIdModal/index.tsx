844caf9926a13c10cb04fc99185cf6dab53108cd
BCJdevelopment <bcj@bcjdevelopment.com>
2024-02-19 00:49:47 -0700

    updated web3modal package, and updated chainId selection

--- src/components/RequireChainIdModal/index.tsx
+++ src/components/RequireChainIdModal/index.tsx
@@ -2,12 +2,20 @@
 
 import { defaultChain } from "@/config/wagmi-config";
 import { Button, Modal, ModalBody, ModalContent, ModalHeader } from "@nextui-org/react";
+import { useWeb3Modal, useWeb3ModalState } from "@web3modal/wagmi/react";
 import { useEffect, useMemo } from "react";
 import { useAccount, useChainId, useSwitchChain } from "wagmi";
 
 export default function RequireChainIdModal() {
   const { isConnected } = useAccount();
-  const chainId = useChainId();
+  const { open } = useWeb3Modal();
+  const wagmiChainId = useChainId();
+  const { selectedNetworkId } = useWeb3ModalState();
+
+  const chainId = useMemo(() => {
+    return selectedNetworkId || wagmiChainId;
+  }, [wagmiChainId, selectedNetworkId]);
+
   const { switchChain, error, isError, isPending } = useSwitchChain();
 
   const wrongNetworkDetected = useMemo(() => {
@@ -50,15 +58,23 @@ export default function RequireChainIdModal() {
             color="primary"
             onPress={() => switchChain({ chainId: defaultChain.id })}
             isLoading={isPending}
+            isDisabled={isError}
           >
             Switch to {defaultChain.name}
           </Button>
-          {isError && (
-            <p className="m-4 text-warning">
+          {isError && (<>
+            <p className="m-4 text-warning text-base">
               Failed to switch networks. To proceed, please manually open your wallet and switch to
-              the {defaultChain.name} network.
+              the {defaultChain.name} network. Or, select a network from below:
             </p>
-          )}
+            <Button
+              className="m-4"
+              color="primary"
+              onPress={() => open({ view: "Networks" })}
+            >
+              Select Network
+            </Button>
+          </>)}
         </ModalBody>
       </ModalContent>
     </Modal>
