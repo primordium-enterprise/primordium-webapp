e54821d126445c59fe89405cb83b611bece99437
BCJdevelopment <bcj@bcjdevelopment.com>
2024-03-10 03:22:58 -0600

    simplifying down the input param state management for creating proposal action

--- src/app/vote/create-proposal/_components/ProposalActionsEditor/components/FunctionOptionInputParams/index.tsx
+++ src/app/vote/create-proposal/_components/ProposalActionsEditor/components/FunctionOptionInputParams/index.tsx
@@ -2,70 +2,69 @@
 
 import Input from "@/components/Input";
 import { AbiFunctionOption } from "../../types";
-import { Dispatch, useEffect, useMemo, useReducer } from "react";
+import { Dispatch, SetStateAction, useEffect, useMemo, useReducer, useState } from "react";
 import { AbiParameter, BaseError, encodeAbiParameters } from "viem";
+import { Textarea as BaseTextArea, extendVariants } from "@nextui-org/react";
+import { getArrayComponents } from "@/utils/abi";
+
+const Textarea = extendVariants(BaseTextArea, {
+  defaultVariants: {
+    maxRows: 3,
+    minRows: 3,
+  },
+});
 
 interface AbiFunctionInputParams {
   [inputIndex: number]: {
-    value: string;
+    values: string[];
     isInvalid?: boolean;
     errorMessage?: string;
   };
 }
 
-const inputParamsReducer = (
-  state: AbiFunctionInputParams,
-  update: { type?: "reset"; payload: AbiFunctionInputParams },
-): AbiFunctionInputParams => {
-  const { type, payload } = update;
-  if (type === "reset") {
-    return payload;
+type AbiFunctionInputParam = {
+  valueItems: AbiFunctionInputParamValueItem[];
+  arrayComponents: ReturnType<typeof getArrayComponents>;
+} & AbiParameter;
+
+interface AbiFunctionInputParamValueItem {
+  value: string;
+  isInvalid?: boolean;
+  errorMessage?: string;
+}
+
+type ParsedAbiInputValue = string | boolean | bigint;
+const parseAbiInputValue = (value: string, abiSingularType: string): ParsedAbiInputValue => {
+  value = value.trim();
+  if (abiSingularType === "string") {
+    return value;
   }
-
-  let newState = { ...state };
-  for (let paramIndex in payload) {
-    newState[paramIndex] = { ...newState[paramIndex], ...payload[paramIndex] };
-  }
-  return newState;
-};
-
-const validateInputParamValue = (
-  functionOption: AbiFunctionOption,
-  inputIndex: number,
-  value: string,
-) => {
-  const arrayComponents = functionOption.arrayComponentsByInputIndex[inputIndex];
-  const type: string = arrayComponents?.[1] || functionOption.inputs[inputIndex].type;
-  console.log(type);
-  let formattedValue: any = value;
-
-  try {
-    console.log(JSON.parse(value));
-    console.log(value);
-  } catch {}
-  if (functionOption.arrayComponentsByInputIndex[inputIndex]) {
-    try {
-      formattedValue = JSON.parse(value);
-      if (!Array.isArray(formattedValue)) {
-        throw new Error("Value must be an array.");
-      }
-    } catch (e: any) {
-      console.log(e);
-      return {
-        isInvalid: true,
-        errorMessage: e?.message ||
-          "Array values must be formatted as a comma seperated list with square brackets.",
-      };
+  if (abiSingularType === "bool") {
+    if (value === "true" || value === "false") {
+      return value === "true" ? true : false;
+    } else {
+      throw new Error("bool values must be 'true' or 'false'.");
     }
   }
-
-  try {
-    encodeAbiParameters([functionOption.inputs[inputIndex]], [formattedValue]);
-    return { isInvalid: false, errorMessage: "" };
-  } catch (e) {
-    return { isInvalid: true, errorMessage: (e as BaseError)?.shortMessage || "There was an error encoding the parameter." };
+  let numericMatch = /^(-?\d+)$/.exec(value);
+  if (numericMatch) {
+    return BigInt(numericMatch[1]);
   }
+  return value;
 };
+
+const inputParamValueValidator = (value: string, abiSingularType: string) => {
+  try {
+    let parsedValue = parseAbiInputValue(value, abiSingularType);
+    encodeAbiParameters([{ type: abiSingularType }], [parsedValue]);
+    return { isInvalid: false, errorMessage: "" };
+  } catch (e: any) {
+    return {
+      isInvalid: true,
+      errorMessage: e?.shortMessage || e?.message || "There was an error parsing the parameter value.",
+    };
+  }
+}
 
 /**
  * Component to input parameters for an ABI function option.
@@ -75,27 +74,30 @@ export default function FunctionOptionInputParams({
 }: {
   functionOption: AbiFunctionOption;
 }) {
-  const [inputParams, dispatchInputParamsUpdate] = useReducer(inputParamsReducer, {});
+  const [inputParams, setInputParams] = useState<AbiFunctionInputParam[]>([]);
 
   const isUnsupported: boolean = useMemo(() => {
-    return functionOption?.inputs.some((param) => param.type.startsWith("tuple")) || false;
+    return functionOption.inputs.some((param) => param.type.startsWith("tuple"));
   }, [functionOption]);
 
   // Reset the inputParams when the function option changes
   useEffect(() => {
-    let payload = {};
+    const newInputParams: AbiFunctionInputParam[] = [];
     if (functionOption) {
-      payload = functionOption.inputs.reduce(
-        (acc, param, index) => ({
-          ...acc,
-          [index]: {
-            value: "",
-          },
-        }),
-        {},
-      );
+      for (let i = 0; i < functionOption.inputs.length; i++) {
+        const abiParam = functionOption.inputs[i];
+        const arrayComponents = getArrayComponents(abiParam.type);
+        const newInputParam: AbiFunctionInputParam = {
+          ...abiParam,
+          arrayComponents,
+          valueItems: arrayComponents
+            ? Array.apply(null, Array(arrayComponents[0] || 1)).map(() => ({ value: "" }))
+            : [{ value: "" }],
+        };
+        newInputParams.push(newInputParam);
+      }
     }
-    dispatchInputParamsUpdate({ type: "reset", payload });
+    setInputParams(newInputParams);
   }, [functionOption]);
 
   useEffect(() => console.log("Input Params", inputParams), [inputParams]);
@@ -107,21 +109,57 @@ export default function FunctionOptionInputParams({
           The selected function uses "tuple" types, which are not currently supported.
         </p>
       ) : (
-        functionOption.inputs.map((param, index) => (
-          <Input
-            key={index}
-            label={`${param.type} ${param.name}`}
-            value={inputParams[index]?.value || ""}
-            errorMessage={inputParams[index]?.errorMessage}
-            onValueChange={(value) => {
-              let payload = {
-                [index]: { value, ...validateInputParamValue(functionOption, index, value) },
-              };
-              dispatchInputParamsUpdate({ payload });
-            }}
-          />
-        ))
+        inputParams.map((inputParam, index) => {
+          return (
+            <AbiFunctionInputParam
+              key={index}
+              inputParam={inputParam}
+              // index={index}
+              setInputParams={setInputParams}
+            />
+          );
+        })
       )}
     </>
   );
+}
+
+function AbiFunctionInputParam({
+  inputParam,
+  // inputParams,
+  // index,
+  setInputParams,
+}: {
+  inputParam: AbiFunctionInputParam;
+  // inputParams: AbiFunctionInputParam[];
+  // index: number;
+  setInputParams: Dispatch<SetStateAction<AbiFunctionInputParam[]>>;
+}) {
+  const [label] = useMemo(() => {
+    return [`${inputParam.type} ${inputParam.name}`, inputParam.type];
+  }, [inputParam]);
+
+  const abiSingularType = useMemo(() => {
+    return inputParam.arrayComponents?.[1] || inputParam.type;
+  }, [inputParam]);
+
+  const BaseComponent = useMemo(
+    () => (inputParam.type.startsWith("string") ? Textarea : Input),
+    [inputParam.type],
+  );
+
+  return inputParam.valueItems.map((item, i) => {
+    return (
+      <BaseComponent
+        key={i}
+        label={label}
+        value={item.value}
+        errorMessage={item.errorMessage}
+        onValueChange={(value) => {
+          inputParam.valueItems[i] = { value, ...inputParamValueValidator(value, abiSingularType)};
+          setInputParams((prev) => [...prev]);
+        }}
+      />
+    );
+  });
 }
